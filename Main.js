1f4f1772c0597576e2f1d430785b6daf79264f1f
Bashar Alwarad <beelwarad52@gmail.com>
2026-02-06 10:24:26 +0100

    addIng: parcel

--- Main.js
+++ Main.js
@@ -1,21 +1,8 @@
-// import App from './src/App.js';
+import App from './src/App.js';
 
-// var root = document.getElementById('root');
+var root = document.getElementById('root');
 
-// var render = async function () {
-//   root.innerHTML = await App();
-// };
-// render();
-
-const users = [
-  { name: 'John', age: 30 },
-  { name: 'Jane', age: 30 },
-  { name: 'Mike', age: 30 },
-];
-
-const strings = JSON.stringify(users);
-
-localStorage.setItem('users', strings);
-
-console.log(users);
-console.log(strings);
+var render = async function () {
+  root.innerHTML = await App();
+};
+render();
